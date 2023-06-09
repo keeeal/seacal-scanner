2e5839166c2f4fa48b20158585b935b279e55cd7
Keeeal <j@keal.au>
2023-06-09 13:52:03 +0930

    add stop button

--- scripts/make-main-scad.sh
+++ scripts/make-main-scad.sh
@@ -1,26 +1,39 @@
+#!/bin/sh
+
 target=src/cad/main.scad
-source=(src/cad/parts/*.scad)
+components=src/cad/components/*.scad
+parts=src/cad/parts/*.scad
 
 echo > $target
 
-for f in ${source[@]}
-do
+for f in $components; do
+    echo "use <components/$(basename $f)>" >> $target
+done
+
+for f in $parts; do
     echo "use <parts/$(basename $f)>" >> $target
 done
 
+echo "use <assembly.scad>" >> $target
 echo >> $target
-echo -n "part = \"assembly\"; // [" >> $target
+echo -n "part = \"assembly\"; // [assembly" >> $target
 
-for f in ${source[@]}
-do
-    echo -n "$(basename $f .scad), " >> $target
+for f in $components; do
+    echo -n ", $(basename $f .scad)" >> $target
 done
 
-sed -i '$ s/..$//' $target
+for f in $parts; do
+    echo -n ", $(basename $f .scad)" >> $target
+done
+
 echo ] >> $target
 echo >> $target
+echo "if (part == \"assembly\") assembly();" >> $target
 
-for f in ${source[@]}
-do
+for f in $components; do
+    echo "if (part == \"$(basename $f .scad)\") $(basename $f .scad | sed 's/-/_/g')();" >> $target
+done
+
+for f in $parts; do
     echo "if (part == \"$(basename $f .scad)\") $(basename $f .scad | sed 's/-/_/g')();" >> $target
 done
